edf76eb26e3ca0dec73e297310fccdd847b40d0b
Moeed1mdnzh <ppro78741@gmail.com>
2023-07-10 21:57:00 -0700

    updated

--- configs.py
+++ configs.py
@@ -1,15 +1,11 @@
+import torch
+
 SHIFT_LIMIT = 0.4
 
 BATCH_SIZE = 128
 
-# import cv2 
-# import numpy as np 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+LOSS_FN = torch.nn.BCELoss()
 
 
-# image = cv2.imread("data.jpg")
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# thresh, bins = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-# kernel = np.ones((1, 1), np.uint8)
-# bins = cv2.morphologyEx(bins, cv2.MORPH_CLOSE, kernel)
-# cv2.imshow("", bins)
-# cv2.waitKey(0)
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from model.generator.generator import Generator
 from model.discriminator.discriminator import Discriminator
 
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
 tfs = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
                                     torchvision.transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
 
@@ -37,10 +38,36 @@ label_paths = list(paths.list_images(os.sep.join(["dataset", "labels"])))
 dataset = ImageDataset(im_paths, label_paths, tfs)
 dataset = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-G_model = Generator()
-D_model = Discriminator()
+G_model = Generator().to(DEVICE)
+D_model = Discriminator().to(DEVICE)
 G_model.apply(init_weights)
 D_model.apply(init_weights)
-print(G_model, D_model)
+
+def d_train(x, discriminator):
+    discriminator.zero_grad()
+    batch_size = x.size(0)
+    d_labels_real = torch.ones(batch_size, 1, DEVICE=DEVICE)
+    d_proba_real = discriminator(x)
+    d_loss_real = loss_fn(d_proba_real, d_labels_real)
+    g_output = generator(x)
+    d_proba_fake = discriminator(g_output)
+    d_labels_fake = torch.zeros(batch_size, 1, DEVICE=DEVICE)
+    d_loss_fake = loss_fn(d_proba_fake, d_labels_fake)
+    d_loss = d_loss_real + d_loss_fake
+    d_loss.backward()
+    d_optimizer.step()
+    return d_loss.data.item()
+
+def g_train(x, generator):
+    generator.zero_grad()
+    batch_size = x.size(0)
+    g_labels_real = torch.ones(batch_size, 1, DEVICE=DEVICE)
+    g_output = generator(x)
+    d_proba_fake = discriminator(g_output)
+    g_loss = loss_fn(d_proba_fake, g_labels_real)
+    g_loss.backward()
+    g_optimizer.step()
+    return g_loss.data.item()
+
 
     
