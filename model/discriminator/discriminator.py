40d64fdab8274187ae96121a306cb3adebbe6539
Moeed1mdnzh <ppro78741@gmail.com>
2023-07-19 01:21:16 +0330

    Updated

--- model/discriminator/discriminator.py
+++ model/discriminator/discriminator.py
@@ -5,27 +5,27 @@ class Discriminator(torch.nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.minibatch_stddev = MinibatchStddev()
-        self.conv_1 = torch.nn.Conv2d(4, 128, (4, 4), stride=(2, 2), padding=1)
+        self.conv_1 = torch.nn.Conv2d(4, 64, (4, 4), stride=(2, 2), padding=1)
         self.act_1 = torch.nn.LeakyReLU(0.2)
         
-        self.conv_2 = torch.nn.Conv2d(128, 256, (4, 4), stride=(2, 2), padding=1)
-        self.bn_2 = torch.nn.BatchNorm2d(256)
+        self.conv_2 = torch.nn.Conv2d(64, 128, (4, 4), stride=(2, 2), padding=1)
+        self.bn_2 = torch.nn.BatchNorm2d(128)
         self.act_2 = torch.nn.LeakyReLU(0.2)
         
-        self.conv_3 = torch.nn.Conv2d(256, 512, (4, 4), stride=(2, 2), padding=1)
-        self.bn_3 = torch.nn.BatchNorm2d(512)
+        self.conv_3 = torch.nn.Conv2d(128, 256, (4, 4), stride=(2, 2), padding=1)
+        self.bn_3 = torch.nn.BatchNorm2d(256)
         self.act_3 = torch.nn.LeakyReLU(0.2)
         
-        self.conv_4 = torch.nn.Conv2d(512, 1024, (4, 4), stride=(2, 2), padding=1)
-        self.bn_4 = torch.nn.BatchNorm2d(1024)
+        self.conv_4 = torch.nn.Conv2d(256, 512, (4, 4), stride=(2, 2), padding=1)
+        self.bn_4 = torch.nn.BatchNorm2d(512)
         self.act_4 = torch.nn.LeakyReLU(0.2)
         
-        self.conv_5 = torch.nn.Conv2d(1024, 2048, (4, 4), stride=(2, 2), padding=1)
-        self.bn_5 = torch.nn.BatchNorm2d(2048)
+        self.conv_5 = torch.nn.Conv2d(512, 1024, (4, 4), stride=(2, 2), padding=1)
+        self.bn_5 = torch.nn.BatchNorm2d(1024)
         self.act_5 = torch.nn.LeakyReLU(0.2)
         
         self.flatten = torch.nn.Flatten()
-        self.linear_0 = torch.nn.Linear(2048*6*6, 1)
+        self.linear_0 = torch.nn.Linear(1024*6*6, 1)
         self.act_0 = torch.nn.Sigmoid()
         
     def forward(self, x):
