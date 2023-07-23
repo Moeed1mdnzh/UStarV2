327bb518d85b9633712b03417b20fe234269a681
Moeed1mdnzh <ppro78741@gmail.com>
2023-07-23 04:05:14 +0330

    Modified discriminator

--- model/discriminator/discriminator.py
+++ model/discriminator/discriminator.py
@@ -11,22 +11,22 @@ class Discriminator(torch.nn.Module):
         self.conv_2 = torch.nn.Conv2d(64, 128, (4, 4), stride=(2, 2), padding=1)
         self.bn_2 = torch.nn.BatchNorm2d(128)
         self.act_2 = torch.nn.LeakyReLU(0.2)
-        self.do_2 = torch.nn.Dropout(0.2)
+        self.do_2 = torch.nn.Dropout(0.1)
         
         self.conv_3 = torch.nn.Conv2d(128, 256, (4, 4), stride=(2, 2), padding=1)
         self.bn_3 = torch.nn.BatchNorm2d(256)
         self.act_3 = torch.nn.LeakyReLU(0.2)
-        self.do_3 = torch.nn.Dropout(0.2)
+        self.do_3 = torch.nn.Dropout(0.1)
         
         self.conv_4 = torch.nn.Conv2d(256, 512, (4, 4), stride=(2, 2), padding=1)
         self.bn_4 = torch.nn.BatchNorm2d(512)
         self.act_4 = torch.nn.LeakyReLU(0.2)
-        self.do_4 = torch.nn.Dropout(0.2)
+        self.do_4 = torch.nn.Dropout(0.1)
         
         self.conv_5 = torch.nn.Conv2d(512, 1024, (4, 4), stride=(2, 2), padding=1)
         self.bn_5 = torch.nn.BatchNorm2d(1024)
         self.act_5 = torch.nn.LeakyReLU(0.2)
-        self.do_5 = torch.nn.Dropout(0.2)
+        self.do_5 = torch.nn.Dropout(0.1)
         
         self.flatten = torch.nn.Flatten()
         self.linear_0 = torch.nn.Linear(1024*6*6, 1)
