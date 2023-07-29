3df3f97d44f3dfedec9e6fc755551243ae1b6c8c
Moeed1mdnzh <ppro78741@gmail.com>
2023-07-30 01:03:07 +0330

    Updated

--- data_generator.py
+++ data_generator.py
@@ -21,7 +21,7 @@ eroded = cv2.erode(dilated, kernel)
 index = 0 # For testing
 augmentor = Augmentor(SHIFT_LIMIT) 
 max_val = len(list(range(0, 186, DATA_CONTROL)))**2
-max_val = (3 * max_val * ((len(list(range(1, 72, DATA_CONTROL)))*28)+(len(list(range(1, 151, DATA_CONTROL)))*28)))+813
+max_val = 3 * max_val * ((len(list(range(1, 72, DATA_CONTROL)))*28)+(len(list(range(1, 151, DATA_CONTROL)))*28))
 pbar = progressbar.ProgressBar(max_value=max_val, widgets=widgets_2)
 for i in range(3):
     channels = [0, 1, 2]
@@ -75,43 +75,5 @@ for i in range(3):
                     for img in cluster:
                         cv2.imwrite(os.sep.join(["dataset", "labels", f"label_{index_2}.jpg"]), img)
                         index_2 += 1
-
-image = cv2.imread(os.sep.join(["dataset", "org.png"]))
-clone = image.copy()
-blurred = cv2.GaussianBlur(image, (5, 5), 0)
-gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-thresh, bins = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-kernel = np.ones((5, 5), np.uint8)
-bins = cv2.morphologyEx(bins, cv2.MORPH_CLOSE, kernel)
-
-b = 0
-SZ = 17
-for i in range(int(255 / SZ)):
-    g = 0
-    for j in range(int(255 / SZ)):
-        r = 0
-        for d in range(int(255 / SZ)):
-            color = [b, g, r]
-            clone[bins==255] = color
-            clone[bins!=255] = [0, 0, 0]
-            if clone.sum() > 45000000:
-                bg = np.full(image.shape, color, dtype=np.uint8)
-                bg[bins!=255] = [0, 0, 0]
-                image[bins!=255] = [0, 0, 0]
-                maximum = np.max(image[bins==255], axis=0)
-                deficit = np.full(image.shape, maximum, dtype=np.float32) - image.astype("float32")
-                res = clone.astype("float32")-deficit
-                res[res < 0] = 0
-                res[res > 255] = 255
-                res = res.astype("uint8")
-                cv2.imwrite(os.sep.join(["dataset", "images", f"img_{index}.jpg"]), cv2.resize(clone, (192, 192)))
-                cv2.imwrite(os.sep.join(["dataset", "labels", f"img_{index}.jpg"]), cv2.resize(res, (192, 192)))
-                index += 1
-                index_2 += 1
-                pbar.update(index, ImageNumber=index)
-            r += SZ
-        g += SZ
-    b += SZ
-    
 pbar.finish()
 print(f"Total number of generated images: {index+index_2}")
